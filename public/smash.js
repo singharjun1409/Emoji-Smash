ba320e7b49514daa8125d2a656418f8c7d35277d
Arjun Singh <as16405@nyu.edu>
2025-05-07 16:14:38 -0400

    Further improvements

--- public/smash.js
+++ public/smash.js
@@ -4,14 +4,29 @@ const grid = document.querySelector(".grid");
 const emoji = document.querySelector(".emoji");
 const scoreElement = document.querySelector(".score");
 const gridPosition = grid.getBoundingClientRect();
-let score = 0;
-
+const easy = document.getElementById("easy");
+const medium = document.getElementById("medium")
+const hard = document.getElementById("hard");
 const cursor = document.querySelector('.cursor')
 
+let score = 0;
+let level = 1000;
+
+// Cursor class will mirror the movements of the actual cursor
 document.addEventListener('mousemove', (e) => {
   cursor.style.left = e.pageX + 'px';
   cursor.style.top = e.pageY + 'px';
 });
+
+// Cursor will tilt down to emulate a smash when the mouse is clicked and then revert
+grid.addEventListener('mousedown', () => {
+    cursor.classList.add('smash');
+});
+  
+grid.addEventListener('mouseup', () => {
+    cursor.classList.remove('smash');
+});
+
 
 function runGame()
 {
@@ -24,40 +39,66 @@ function runGame()
 
 function setup(evt)
 {
-    evt.preventDefault();
     const icon = document.querySelector("#emojiSelect").value;
     console.log(`Emoji selected ${icon}`);
     emoji.textContent = icon;
     emoji.removeEventListener("click" , emojiClickHandler);
     emoji.addEventListener("click" , emojiClickHandler);
     emojiClickHandler(0);
-    const interval = setInterval(runGame , 1000);
+    const interval = setInterval(runGame , level);
    
-
     setTimeout(() =>
         {
             clearInterval(interval);
             console.log("Game Over");
             emoji.removeEventListener("click" , emojiClickHandler);
+            easy.disabled = false;
+            medium.disabled = false;
+            hard.disabled = false;
         }, 10000);
-
+    
     
 }
 
 function emojiClickHandler(newScore)
-{ 
-    // Check if the argument is a number (newScore) or an event object
+{     
     if (typeof newScore === "number") {
-        score = newScore; // Set the score to the provided value
-    } else {
-        score += 1; // Increment the score
+        score = newScore; 
+    } 
+    else 
+    {
+        score += 1; 
     }
+
     scoreElement.setAttribute("value" , score);
     scoreElement.textContent = `Score: ${score}`;
     
 }
 
-emojiForm.addEventListener('submit' , setup);
+
+easy.addEventListener("click" , () =>
+{
+    level = 1000;
+    medium.disabled = true;
+    hard.disabled = true;
+    setup();
+})
+
+medium.addEventListener('click' , () =>
+{
+    level = 900;
+    easy.disabled = true;
+    hard.disabled = true;
+    setup();
+})
+
+hard.addEventListener("click" , () =>
+{
+    level = 800;
+    easy.disabled = true;
+    medium.disabled = true;
+    setup();
+})
 
 
 
